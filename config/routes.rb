64947f1698870a93a1c09c8a735daa15876d7f4f
Jake Pham <jakepham90@gmail.com>
2013-05-31 05:57:54 -0700

    Start on the layout for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Profile::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +50,7 @@ Profile::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => "home#index"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
